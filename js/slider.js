91132e4fda6d093bbaddf9cca9b35d838c65d318
Muhammad Awais <muhammadawais03484951435@gmail.com>
2024-03-26 11:20:25 +0500

    Slider code added

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//     var slides = document.getElementsByClassName("slide");
+//     var radios = document.getElementsByName("r");
+//     for (var i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//         radios[i].checked = false;
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1;
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     radios[slideIndex - 1].checked = true;
+//     setTimeout(showSlides, 2000); // Change slide every 2 seconds
+// }
